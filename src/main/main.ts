99f22eb5e215f04b2a7c075e51abce07f8b59000
jenson2525 <1515356530@qq.com>
2026-05-04 23:13:05 +0800

    first commit

---
+++ src/main/main.ts
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+function createWindow(): void {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  // win.loadFile(path.join(__dirname, '../../src/renderer/index.html'));
+  win.loadFile(path.join(__dirname, '../renderer/index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
---
+++ src/renderer/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Celestia</h1>
+      <p>欢迎使用 Celestia 游戏启动台</p>
+    </div>
+  );
+};
+
+export default App;
